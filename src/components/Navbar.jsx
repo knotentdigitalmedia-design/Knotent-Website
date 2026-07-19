2b1c17a9b654e5b7326df2508232780241515e72
bhavyaffdl-enge <bhavyaffdl@gmail.com>
2026-07-19 10:26:33 +0530

    style: change navbar brand font to Montserrat Bold

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,7 @@ function Navbar() {
       <div className="navbar__inner">
         <NavLink to="/" className="navbar__brand" onClick={() => setOpen(false)}>
           <img src={logo} alt="Knotent Logo" className="navbar__logo-img" style={{ height: '40px', objectFit: 'contain' }} />
-          <span style={{ fontSize: '24px', fontWeight: 'bold', letterSpacing: '2px', lineHeight: 1 }}>
+          <span style={{ fontSize: '24px', fontFamily: 'Montserrat, sans-serif', fontWeight: 700, letterSpacing: '2px', lineHeight: 1 }}>
             <span style={{ color: '#F50615' }}>KNOT</span>
             <span style={{ color: '#FFFFFF' }}>ENT</span>
           </span>
