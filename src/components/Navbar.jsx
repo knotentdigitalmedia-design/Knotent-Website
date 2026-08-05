dcce47a0117641bc8a8ba4d4a146f8f25d42002a
bhavyaffdl-enge <bhavyaffdl@gmail.com>
2026-08-05 11:22:48 +0530

    Revert "style: align logo and KNOTENT brand to left in navbar"
    This reverts commit fa1acbab0aec5e214dd51fcbffcca8819afbd0a4.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,9 +24,9 @@ function Navbar() {
       transition={{ type: 'spring', stiffness: 200, damping: 20 }}
     >
       <div className="navbar__inner">
-        <NavLink to="/" className="navbar__brand" onClick={() => setOpen(false)} style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start', margin: 0, padding: 0 }}>
-          <img src={logo} alt="Knotent Logo" className="navbar__logo-img" style={{ height: '40px', objectFit: 'contain', display: 'block', margin: 0 }} />
-          <span style={{ fontSize: '24px', fontFamily: 'Montserrat, sans-serif', fontWeight: 700, letterSpacing: '2px', lineHeight: 1, margin: 0 }}>
+        <NavLink to="/" className="navbar__brand" onClick={() => setOpen(false)}>
+          <img src={logo} alt="Knotent Logo" className="navbar__logo-img" style={{ height: '40px', objectFit: 'contain' }} />
+          <span style={{ fontSize: '24px', fontFamily: 'Montserrat, sans-serif', fontWeight: 700, letterSpacing: '2px', lineHeight: 1 }}>
             <span style={{ color: '#F50615' }}>KNOT</span>
             <span style={{ color: '#FFFFFF' }}>ENT</span>
           </span>
