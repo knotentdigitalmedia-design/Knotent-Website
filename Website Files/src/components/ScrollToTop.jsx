e985af107d34688df0084f405715ceef73bcc7ff
bhavyaffdl-enge <bhavyaffdl@gmail.com>
2026-08-21 12:17:14 +0530

    Simplify ScrollToTop

--- Website Files/src/components/ScrollToTop.jsx
+++ Website Files/src/components/ScrollToTop.jsx
@@ -1,19 +1,12 @@
 import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
-import { useLenis } from 'lenis/react'
 
 export default function ScrollToTop() {
   const { pathname } = useLocation()
-  const lenis = useLenis()
 
   useEffect(() => {
-    // Standard browser scroll
     window.scrollTo(0, 0)
-    // Lenis scroll if active
-    if (lenis) {
-      lenis.scrollTo(0, { immediate: true })
-    }
-  }, [pathname, lenis])
+  }, [pathname])
 
   return null
 }
